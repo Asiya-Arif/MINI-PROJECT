c03156fe9980fa853719dffccfa10dec878b6ac0
Asiya Arif <itsasiyaarif@gmail.com>
2024-02-04 07:24:25 +0530

    done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,58 @@
 'use strict';
 
+    /**
+     * navbar toggle
+     */
 
+    const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+    const navbar = document.querySelector("[data-navbar]");
+    const navCloseBtn = document.querySelector("[data-nav-close-btn]");
 
-/**
- * navbar toggle
- */
+    const navElemArr = [navOpenBtn, navCloseBtn];
 
-const navOpenBtn = document.querySelector("[data-nav-open-btn]");
-const navbar = document.querySelector("[data-navbar]");
-const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+    for (let i = 0; i < navElemArr.length; i++) {
+        navElemArr[i].addEventListener("click", function () {
+            navbar.classList.toggle("active");
+        });
+    }
 
-const navElemArr = [navOpenBtn, navCloseBtn];
+    /**
+     * toggle navbar when click any navbar link
+     */
 
-for (let i = 0; i < navElemArr.length; i++) {
-  navElemArr[i].addEventListener("click", function () {
-    navbar.classList.toggle("active");
-  });
-}
+    const navbarLinks = document.querySelectorAll("[data-nav-link]");
 
-/**
- * toggle navbar when click any navbar link
- */
+    for (let i = 0; i < navbarLinks.length; i++) {
+        navbarLinks[i].addEventListener("click", function () {
+            navbar.classList.remove("active");
+        });
+    }
 
-const navbarLinks = document.querySelectorAll("[data-nav-link]");
+    document.addEventListener('DOMContentLoaded', function () {
+        // Find the donate button
+        var donateButton = document.querySelector('.btn-primary');
 
-for (let i = 0; i < navbarLinks.length; i++) {
-  navbarLinks[i].addEventListener("click", function () {
-    navbar.classList.remove("active");
-  });
-}
+        // Attach click event listener to the donate button
+        donateButton.addEventListener('click', function () {
+            // Redirect to the payment.html page
+            window.location.href = 'payment.html';
+        });
+    });
 
+    /**
+     * header active when window scrolled down
+     */
 
+    const header = document.querySelector("[data-header]");
 
+    window.addEventListener("scroll", function () {
+        window.scrollY >= 50 ? header.classList.add("active")
+            : header.classList.remove("active");
+    });
 
+    /**
+     * processDonation function
+     */
 
-/**
- * header active when window scrolled down
- */
-
-const header = document.querySelector("[data-header]");
-
-window.addEventListener("scroll", function () {
-  window.scrollY >= 50 ? header.classList.add("active")
-    : header.classList.remove("active");
-});
+    
+    
